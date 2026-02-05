10634706a0247af125ad31da9302805000742bce
ahmadfaruqi1211-lgtm <ahmad.faruqi1211@gmail.com>
2026-02-05 22:33:05 +0500

    fix(build): update vite config to build from root instead of web dir

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,12 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  root: 'web',
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: '../js', dest: '' },
-        { src: '../css', dest: '' },
-        { src: '../icons', dest: '' },
-        { src: '../manifest.json', dest: '' },
-        { src: '../browserconfig.xml', dest: '' },
-        { src: '../pwa-192x192.png', dest: '' },
-        { src: '../pwa-512x512.png', dest: '' }
-      ]
-    })
-  ],
+  // Use current directory as root
+  root: '.',
+  // simple static file serving
+  publicDir: 'public',
   build: {
-    outDir: '../mobile',
-    emptyOutDir: true
+    outDir: 'dist',
+    emptyOutDir: true,
   }
 })
