794f2e2b64228b41879e7823c1e0fb30b3eb9a4f
ahmadfaruqi1211-lgtm <ahmad.faruqi1211@gmail.com>
2026-02-06 11:30:26 +0500

    fix(build): add static copy plugin to include JS/CSS/icons in dist

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   // Use current directory as root
   root: '.',
   // simple static file serving
   publicDir: 'public',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        { src: 'js', dest: '' },
+        { src: 'css', dest: '' },
+        { src: 'icons', dest: '' },
+        { src: 'manifest.json', dest: '' },
+        { src: 'browserconfig.xml', dest: '' },
+        { src: 'pwa-192x192.png', dest: '' },
+        { src: 'pwa-512x512.png', dest: '' }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
